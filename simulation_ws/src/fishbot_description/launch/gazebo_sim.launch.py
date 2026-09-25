fe2ac852f86275afd0dab5ca9eab7ac17c51926b
moke <moke@example.com>
2026-09-25 20:19:43 +0800

    feat: 添加gazebo仿真启动文件和自定义房间世界
    新增gazebo_sim.launch.py用于启动gazebo仿真环境，新增custom_room.world自定义房间模型，修改CMakeLists.txt安装world目录

---
+++ simulation_ws/src/fishbot_description/launch/gazebo_sim.launch.py
@@ -0,0 +1,41 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+import os 
+
+def generate_launch_description():
+    #获取固定的urdf文件路径
+    urdf_package_path = get_package_share_directory("fishbot_description")
+    default_xacro_path = os.path.join(urdf_package_path, "urdf", "fishbot", "fishbot.urdf.xacro")
+    # default_rviz_config_path = os.path.join(urdf_package_path, "config", "display_robot_model.rviz")
+    default_gazebo_world_path = os.path.join(urdf_package_path, "world", "custom_room.world")
+    #声明一个urdf目录的参数，方便修改
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name = "model",
+        default_value=str(default_xacro_path),
+        description="加载的模型文件路径",
+    )
+    #通过文件路径，获取内部内容，并转换成参数值对象，以供传入 robot_state_publisher 节点
+    substitutions_command_result  = launch.substitutions.Command(["xacro ", launch.substitutions.LaunchConfiguration("model")])
+    robot_description_value = ParameterValue(substitutions_command_result, value_type=str)
+    
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description_value}],
+        name="robot_state_publisher",
+        output="screen",
+    )
+    action_launch_gazebo = launch.actions.IncludeLaunchDescription(
+        launch_description_source=launch.substitutions.PathJoinSubstitution(
+            [get_package_share_directory("gazebo_ros"), "launch", "gazebo.launch.py"]
+        ),
+        launch_arguments=[("world", default_gazebo_world_path), ("verbose", "true")],
+    )
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path,
+        action_robot_state_publisher,
+        action_launch_gazebo,
+
+    ])
